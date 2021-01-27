543e12ba123b7d47be854b3fdf400da2a8069016
agu <agu@pop-os.localdomain>
2021-01-27 14:09:06 +0100

    base

---
+++ js/app.js
@@ -0,0 +1,36 @@
+"use strict"
+console.log('stationery');
+
+let products = 
+[
+    {
+        "picture": "../sklep/pictures/pencils.jpg",
+        "name": "6 Color Pencils",
+        "price": "8,50zł",
+        "color": "more",
+        "material": "wood, pastel",
+        "size": "17cmx10cm",
+        "quantity": "6 elements"
+    },
+    {
+        "picture": "../sklep/pictures/ruler.jpg",
+        "name": "Ruler",
+        "price": "4,80zł",
+        "color": "transparent",
+        "material": "plastic",
+        "size": "20cmx4cm",
+        "quantity": "1"
+    },
+    {
+        "picture": "../sklep/pictures/notebook.jpg",
+        "name": "Notebook",
+        "price": "3,40zł",
+        "color": "pink/bird",
+        "material": "paper",
+        "size": "A5 14.8cmx21cm",
+        "quantity": "30 pages"
+    }
+];
+
+JSON.parse();
+
---
+++ podstronka/js/app.js
@@ -0,0 +1,22 @@
+"use strict"
+console.log('shop');
+
+JSON.parse([{"name": "6 Color Pencils",
+        "price": "8,50zł",
+        "color": "more",
+        "material": "wood, pastel",
+        "size": "17cmx10cm",
+        "quantity": "6 elements"},
+    {"name": "Ruler",
+        "price": "4,80zł",
+        "color": "transparent",
+        "material": "plastic",
+        "size": "20cmx4cm",
+        "quantity": "1"},
+    {"name": "Notebook",
+        "price": "3,40zł",
+        "color": "pink/bird",
+        "material": "paper",
+        "size": "A5 14.8cmx21cm",
+        "quantity": "30 pages"}])
+
