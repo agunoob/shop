c8145fce3635ffff5ca772442cf8214b2916aa26
agu <agu@pop-os.localdomain>
2021-02-04 15:48:22 +0100

    modal display, folder rearrangement

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@
 let test = { 
     "products":[
         {
-            "picture": "../sklep/pictures/pencils.jpg",
+            "gallery": [
+                {
+                    "id": 0,
+                    "pic": "../sklep/pictures//pencils/pencils.jpg"
+                },
+                {
+                    "id": 1,
+                    "pic": "../sklep/pictures/pencils/pencils2.jpg"
+                },
+                {
+                    "id": 2,
+                    "pic": "../sklep/pictures/pencils/pencils3.jpg"
+                }
+            ],
             "name": "6 Color Pencils",
             "price": "8,50zł",
             "color": "more",
@@ -11,10 +24,23 @@ let test = {
             "size": "17cmx10cm",
             "quantity": "6 elements",
             "available": true,
-            "add": "../sklep/icons/plus.png"
+            "addToCartModal": "add to cart",
         },
-        {
-            "picture": "../sklep/pictures/ruler.jpg",
+        { 
+            "gallery": [
+                {
+                    "id": 0,
+                    "pic": "../sklep/pictures/ruler/ruler.jpg"
+                },
+                {
+                    "id": 1,
+                    "pic": "../sklep/pictures/ruler/ruler2.jpg"
+                },
+                {
+                    "id": 2,
+                    "pic": "../sklep/pictures/ruler/ruler3.jpeg"
+                }
+            ],
             "name": "Ruler",
             "price": "4,80zł",
             "color": "transparent",
@@ -22,10 +48,23 @@ let test = {
             "size": "20cmx4cm",
             "quantity": "1",
             "available": false,
-            "add": "../sklep/icons/plus.png"
+            "addToCartModal": "add to cart",
         },
         {
-            "picture": "../sklep/pictures/notebook.jpg",
+            "gallery": [
+                {
+                    "id": 0,
+                    "pic": "../sklep/pictures/notebook/notebook.jpg"
+                },
+                {
+                    "id": 1,
+                    "pic": "../sklep/pictures/notebook/notebook2.jpg"
+                },
+                {
+                    "id": 2,
+                    "pic": "../sklep/pictures/notebook/notebook3.jpeg"
+                }
+            ],
             "name": "Notebook",
             "price": "3,40zł",
             "color": "pink/bird",
@@ -33,7 +72,7 @@ let test = {
             "size": "A5 14.8cmx21cm",
             "quantity": "30 pages",
             "available": true,
-            "add": "../sklep/icons/plus.png"
+            "addToCartModal": "add to cart",
         }
     ]
 };
@@ -56,110 +95,118 @@ let myObj = JSON.parse(getIt);
 
 //stworz div z klasą i wyswietl dla kazdego [i]
 for (let i in myObj.products) {
-    let createDiv = document.createElement('div');
+    const createDiv = document.createElement('div');
     let newDiv= document.getElementById('products-box').appendChild(createDiv);
     newDiv.classList.add('product');
     //klik w ten [i] div modal zaby pokazac wiekszy opis
     newDiv.onclick = function showMore () {
         //stworz modal background
-        let createDivModal = document.createElement('div');
+        const createDivModal = document.createElement('div');
         let divModal= document.body.appendChild(createDivModal);
         divModal.classList.add('modal-background');
-        // document.querySelector('.modal-background').style.visibility = 'visible';
         //div modal
-        let createDivShowMore = document.createElement('div');
+        const createDivShowMore = document.createElement('div');
         let divShowMore= createDivModal.appendChild(createDivShowMore);
         divShowMore.classList.add('more');
         //wyswietl przycisk exit
-        let createPExit = document.createElement('p');
-        let PExit= divShowMore.appendChild(createPExit);
-        PExit.classList.add('exit');
-        PExit.innerHTML= 'x';
-        //schowaj
-        PExit.onclick = function hide() {
-        console.log('schowaj sie błagam');
+        const createImgExit = document.createElement('img');
+        let ImgExit= divShowMore.appendChild(createImgExit);
+        ImgExit.classList.add('exit');
+        ImgExit.src= "../sklep/pictures/icons/x-mark.png";
+        //schowaj, click w x
+        ImgExit.onclick = function hide() {
         divModal.style.display= 'none';
         }
-        //wyswietl zdjecie
-        let createModalImg = document.createElement('img');
-        let modalImg= divShowMore.appendChild(createModalImg);
-        modalImg.classList.add('description', 'photo');
-        modalImg.src= myObj.products[i].picture;
+        //schowaj, click poza modal
+        window.onclick = function (event) {
+            if (event.target == divModal) {
+              divModal.style.display = 'none';
+            }
+        }
+
+        // //wyswietl zdjecie
+        for (let j in myObj.products[i].gallery) {
+            const createModalImg = document.createElement('img');
+            let modalImg= divShowMore.appendChild(createModalImg);
+            modalImg.classList.add('description', 'letters-more', 'photo-modal');
+            modalImg.src= myObj.products[i].gallery[0].pic;
+            console.log('wyswietlam pierwsze zdjecie');
+            modalImg.onclick = function () {
+                for (let j=1; j<myObj.products[i].gallery.length; j++) {
+                    console.log(j);
+                    modalImg.src= myObj.products[i].gallery[j].pic;
+                }
+            }
+        }
+
         //wyswietl nazwe
-        let createPNext = document.createElement('p');
+        const createPNext = document.createElement('p');
         let PNext= divShowMore.appendChild(createPNext);
-        PNext.classList.add('description', 'letters', 'letters-more');
-        PNext.innerHTML= 'name: ' + myObj.products[i].name;
+        PNext.classList.add('description', 'name-grid');
+        PNext.innerHTML= myObj.products[i].name;
         //wyswietl cene
-        let createAnotherPNext = document.createElement('p');
+        const createAnotherPNext = document.createElement('p');
         let AnotherPNext= divShowMore.appendChild(createAnotherPNext);
-        AnotherPNext.classList.add('description', 'letters', 'letters-more');
+        AnotherPNext.classList.add('description', 'letters-more');
         AnotherPNext.innerHTML= 'price: ' + myObj.products[i].price;
         //wyswietl kolor
-        let createPA = document.createElement('p');
+        const createPA = document.createElement('p');
         let pA= divShowMore.appendChild(createPA);
-        pA.classList.add('description', 'letters', 'letters-more');
+        pA.classList.add('description', 'letters-more');
         pA.innerHTML= 'color: ' + myObj.products[i].color;
         //wyswietl material
-        let createPB = document.createElement('p');
+        const createPB = document.createElement('p');
         let pB= divShowMore.appendChild(createPB);
-        pB.classList.add('description', 'letters', 'letters-more');
+        pB.classList.add('description', 'letters-more');
         pB.innerHTML= 'material: ' + myObj.products[i].material;
         //wyswietl rozmiar
-        let createPC = document.createElement('p');
+        const createPC = document.createElement('p');
         let pC= divShowMore.appendChild(createPC);
-        pC.classList.add('description', 'letters', 'letters-more');
+        pC.classList.add('description', 'letters-more');
         pC.innerHTML= 'size: ' + myObj.products[i].size;
         //wyswietl zawartosc
-        let createPD = document.createElement('p');
+        const createPD = document.createElement('p');
         let pD= divShowMore.appendChild(createPD);
-        pD.classList.add('description', 'letters', 'letters-more');
+        pD.classList.add('description', 'letters-more');
         pD.innerHTML= 'quantity: ' + myObj.products[i].quantity;
+        // wyswietl przycisk add-modal 
+        const createAddModal = document.createElement('p');
+        let addModal= divShowMore.appendChild(createAddModal);
+        addModal.classList.add('description', 'add-modal');
+        addModal.innerHTML= myObj.products[i].addToCartModal;
         //wyswietl dostepnosc
-        let createPE = document.createElement('p');
+        const createPE = document.createElement('p');
         let pE= divShowMore.appendChild(createPE);
-        pE.classList.add('description', 'letters', 'letters-more');
+        pE.classList.add('description', 'letters-more');
         if (myObj.products[i].available === true) {
-            pE.innerHTML= 'is it available: yes';
+            pE.innerHTML= 'available: yes';
         } else {
-            pE.innerHTML= 'is it available: no';
+            pE.innerHTML= 'available: no';
+            addModal.style.backgroundColor='lightgrey';
+            addModal.style.boxShadow='none';
+            addModal.style.border='none';
+            addModal.style.color='grey';
+            addModal.style.cursor='not-allowed';
         }
-        // wyswietl przycisk add 
-        let createAnotherImg = document.createElement('img');
-        let ImgNext= divShowMore.appendChild(createAnotherImg);
-        ImgNext.classList.add('description', 'add');
-        ImgNext.src= myObj.products[i].add;
-        };
+    };
     //stworz img produktu z klasa i wyswietl (zdjecie)
-    let createImg = document.createElement('img');
+    const createImg = document.createElement('img');
     let newImg= newDiv.appendChild(createImg);
     newImg.classList.add('description', 'photo');
-    newImg.src= myObj.products[i].picture;
+    newImg.src= myObj.products[i].gallery[0].pic;
     //stworz p z klasa i wyswietl (nazwa)
-    let createP = document.createElement('p');
+    const createP = document.createElement('p');
     let newP= newDiv.appendChild(createP);
     newP.classList.add('description', 'letters');
     newP.innerHTML= myObj.products[i].name;
     //stworz p z klasa i wyswietl (cena)
-    let createAnotherP = document.createElement('p');
+    const createAnotherP = document.createElement('p');
     let newAnotherP= newDiv.appendChild(createAnotherP);
     newAnotherP.classList.add('description', 'letters');
     newAnotherP.innerHTML= myObj.products[i].price;
     // stworz img z przyciskiem z klasa do kazdego [i]
-    let createAnotherImg = document.createElement('img');
+    const createAnotherImg = document.createElement('img');
     let newAnotherImg= newDiv.appendChild(createAnotherImg);
     newAnotherImg.classList.add('description', 'add');
-    newAnotherImg.src= myObj.products[i].add;
-
-        //klik w img zaby dodać do koszyka  ???????????????????????
-        // newAnotherImg.onclick = function addToCart () {
-        //     console.log('w to tez klikłem');
-        // };
-
-    // for (let j in myObj.products[i]) {
-    //     let createP = document.createElement('p');
-    //     let newP= newDiv.appendChild(createP);
-    //     newP.classList.add('description');
-    //     newP.innerHTML= myObj.products[i][j];
-    // }
+    newAnotherImg.src= "../sklep/pictures/icons/plus.png";
 }
